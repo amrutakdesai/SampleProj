22003c21a03ec5ec82615e988862f853960e9df3
amrutakdesai <97360236+amrutakdesai@users.noreply.github.com>
2022-05-16 18:06:22 +0530

    Update i.java

--- src/test/java/Webdriver/ScreenshotAndJavascript/i.java
+++ src/test/java/Webdriver/ScreenshotAndJavascript/i.java
@@ -4,8 +4,7 @@ public class i {
 
 	public static void main(String ar[])
 	{
-		System.out.println("hello welcome1 to Capgemini");
-				System.out.println("hello jjh to hghghh");
+int n;				System.out.println("hello jjh to hghghh");
 
 	}
 }
