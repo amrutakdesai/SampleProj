9709ea91004d56d8e44dfd55c2352cf39f7eccfb
unknown <amrutakdesai1008@gmail.com>
2022-05-16 17:28:00 +0530

    Adding new File

---
+++ src/test/java/Webdriver/ScreenshotAndJavascript/i.java
@@ -0,0 +1,11 @@
+package Webdriver.ScreenshotAndJavascript;
+
+public class i {
+
+	public static void main(String ar[])
+	{
+		System.out.println("hello welcome1 to Capgemini");
+				System.out.println("hello welcome1 to Capgemini");
+
+	}
+}
