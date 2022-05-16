cfad90ee62d8beff89b05641ca2448f3394aaa4e
unknown <amrutakdesai1008@gmail.com>
2022-05-16 18:08:39 +0530

    hhghghgdhgh

--- src/test/java/Webdriver/ScreenshotAndJavascript/mass.java
+++ src/test/java/Webdriver/ScreenshotAndJavascript/mass.java
@@ -4,7 +4,7 @@ public class mass {
 
 	public static void main(String ar[])
 	{
-		System.out.println("hello welcome1 to Capgemini");
+				System.out.println("hi how are you");
 				System.out.println("hello welcome1 to Capgemini");
 
 	}
