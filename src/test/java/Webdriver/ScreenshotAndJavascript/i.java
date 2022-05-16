901c0fb1c758d69bec99980c42fffcb84b960aee
amrutakdesai <97360236+amrutakdesai@users.noreply.github.com>
2022-05-16 17:59:35 +0530

    Update i.java

--- src/test/java/Webdriver/ScreenshotAndJavascript/i.java
+++ src/test/java/Webdriver/ScreenshotAndJavascript/i.java
@@ -5,7 +5,7 @@ public class i {
 	public static void main(String ar[])
 	{
 		System.out.println("hello welcome1 to Capgemini");
-				System.out.println("hello welcome1 to Capgemini");
+				System.out.println("hello welcome1 to hghghh");
 
 	}
 }
